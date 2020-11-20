9f3fd521fb2724ab55abc7b68627676d584b86c6
dolorsy <47667168+dolorsy@users.noreply.github.com>
2020-11-20 09:46:36 +0200

    first Commit

---
+++ app/src/main/java/com/dolor/destroyordefense/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dolor.destroyordefense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
